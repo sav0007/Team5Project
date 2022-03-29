19f79893a021cfc0a56910e746e06128f4ef5bb1
will <wew0011@uah.edu>
2022-03-29 11:28:55 -0500

    Movie Database model class created and added addMovie function so we can start parsing data

---
+++ Model/MovieDatabase.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+
+
+public class MovieDatabase
+{
+    private ArrayList<Movie> movies;
+
+    public void addMovie(Movie movie)
+    {
+        movies.add(movie);
+    }
+
+
+
+
+
+}
