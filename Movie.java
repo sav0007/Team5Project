87dd5b8c9029bfc39c2ff6433a36ba2bd013378c
will <wew0011@uah.edu>
2022-03-29 10:11:38 -0500

    Movie Model Class

--- Movie.java
+++ Movie.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Movie
 {
-
+    private String title;
+    private String mpaa;
+    private Date publishDate;
+    private String runTime;
+    private ArrayList<String> genre;
 }
