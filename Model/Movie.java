5dc4a61c3c0927defca4e44a355e0136bd61a39d
will <wew0011@uah.edu>
2022-03-29 11:19:57 -0500

    Movie Model Class Added Review Functions

--- Model/Movie.java
+++ Model/Movie.java
@@ -21,6 +21,9 @@ public class Movie
     private String ratings;
     private ArrayList<String> userReviews;
 
+    /**
+     * default constructor not 100% sure its needed though
+     */
     Movie()
     {
         title = "unknown";
@@ -36,6 +39,23 @@ public class Movie
         //poster;
         ratings = "unknown rating";
     }
+
+    /**
+     *  parameteriezed constructor
+     * @param Title movie title
+     * @param Mpaa mpaa rating
+     * @param Date1 date
+     * @param Runtime movie runtime
+     * @param Genre list of genres for a given movie
+     * @param Directors list of directors for a movie
+     * @param Actors list of actors for a movie
+     * @param Plot movie plot
+     * @param Languages languages a movie is in
+     * @param Country country movie is in
+     * @param Awards awards movie has recieved
+     * @param Poster image for a movie
+     * @param Ratings movie ratings idk if this supposed to be user ratings so im commenting it out
+     */
     Movie(String Title, String Mpaa, Date Date1, String Runtime, ArrayList<String> Genre, ArrayList<String> Directors, ArrayList<String> Actors, String Plot, ArrayList<String> Languages, String Country, ArrayList<String> Awards, Image Poster, String Ratings)
     {
         title = Title;
@@ -49,8 +69,30 @@ public class Movie
         country = Country;
         awards = Awards; // listed as just a string in document
         poster = Poster;
-        ratings = Ratings;
+        //ratings = Ratings;
     }
 
+    /**
+     * adds a user review to a movie
+     * @param review new review to be added
+     */
+    void AddReview(String review)
+    {
+        userReviews.add(review);
+    }
 
+    /**
+     * edits a movie reviews entry
+     * @param i index of review to be edited
+     * @param review edited review
+     */
+    void EditReview(int i, String review)
+    {
+        userReviews.set(i, review);
+    }
+
+    void DeleteReview(int i)
+    {
+        userReviews.remove(i);
+    }
 }
