96b4888bd4f217ff0c3db4a0eef155a335d09d5d
leela <lmh0030@uah.edu>
2022-03-31 10:11:46 -0500

    editing view classes

--- View/MainPage.java
+++ View/MainPage.java
@@ -7,5 +7,29 @@ import java.awt.geom.*;
 import javax.swing.*;
 
 public class MainPage {
+    static public void createFrame() {
+        JFrame frame = new JFrame();
+
+        frame.setSize(640,640);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    static public JPanel drawTopBar() {
+        JPanel topBar = new JPanel();
+
+        // code here
+
+        return topBar;
+    }
+
+    static public JPanel drawHomePanel() {
+        JPanel homePanel = new JPanel();
+
+        // code here
+
+        return homePanel;
+    }
 
 }
