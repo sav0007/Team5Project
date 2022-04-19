3126fa03ccb1b27ae21639b5139cdaa32ddfa98b
TheSouthernComfort <98067321+sav0007@users.noreply.github.com>
2022-04-19 14:50:59 -0500

    Add files via upload
    Had trouble with this. So it may take some tweaking when it comes to debugging

---
+++ Model/MovieSearch.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class MovieSearch extends MovieDatabase{
+
+    private String movietitle;
+    MovieDatabase searchResults;
+    public MovieSearch(String title){
+        title = movietitle;
+    }
+
+    public MovieDatabase Search(String Title, String Mpaa, String Date1, String Runtime, String Genre, String Directors, String Actors, String Plot, String Languages, String Country, String Awards, String Poster, String Ratings){
+        Movie o_movie = new Movie (Title,Mpaa,Date1,Runtime,Genre,Directors,Actors,Plot,Languages,Country,Awards,Poster, Ratings);
+        while(movies != null){
+            if(movies.contains(o_movie)){
+                searchResults.addMovie(o_movie);
+                return searchResults;
+            }
+        }
+        return null;
+    }
+}
