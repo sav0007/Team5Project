e14afb07a02436fc6e3bb49ba7f2f8ee0d3446f8
TheSouthernComfort <98067321+sav0007@users.noreply.github.com>
2022-04-20 23:26:33 -0500

    Update Login.java
    logOut now saves storesProfile before logging out. createUser function created which stores the user in the hash map, etc...

--- Model/Login.java
+++ Model/Login.java
@@ -14,11 +14,25 @@ public class Login {
     private static final String UsersDatabaseFilename = "UserList.json";
     private static UserProfile CurrentUser;
     private static Map<String, LoginInfo> Users;
-
+    
     public static UserProfile getCurrentUser() {
         return CurrentUser;
     }
+    public static void createAccount(String username, String pass){
 
+        final LoginInfo user = Users.get(username);
+        if (user == null) {
+            final LoginInfo newUser = new LoginInfo(username, pass);
+            Users.put(username, newUser);
+
+            final ArrayList<LoginInfo> users = returnUserList();
+            users.add(newUser);
+
+            final UserProfile profile = new UserProfile(username, pass);
+            profile.storeProfile();
+        }
+
+    }
     public static boolean checkInfo(final String username, final String password){
         final LoginInfo user = Users.get(username);
 
@@ -33,8 +47,8 @@ public class Login {
         return false;
     }
 
-    // TODO: Ask about saving preferences and collections to Will.
     public static void logout(){
+        CurrentUser.storeProfile();
         CurrentUser = null;
     }
 
@@ -53,7 +67,24 @@ public class Login {
         }
         return null;
     }
-
+    public static ArrayList<LoginInfo> returnUserList(){
+        final Gson gson = new Gson();
+        final JsonParser jsonParser = new JsonParser();
+        try {
+            final FileReader reader = new FileReader(UsersDatabaseFilename);
+            final Object obj = jsonParser.parse(reader);
+            final JsonArray loginInfoJsonObj = (JsonArray) obj;
+            final ArrayList<LoginInfo> users = new ArrayList<LoginInfo>();
+            for (int i = 0; i < loginInfoJsonObj.size(); i++) {
+                final LoginInfo user = gson.fromJson(loginInfoJsonObj.get(i), LoginInfo.class);
+                users.add(user);
+            }
+            return users;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     public static void loadUserList(){
         final Gson gson = new Gson();
 
@@ -72,7 +103,27 @@ public class Login {
             e.printStackTrace();
         }
     }
+    public static void saveUserList(ArrayList<LoginInfo> users){
+        for (int i =0; i < users.size(); i++)
+        {
+            Gson gson1 = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .disableHtmlEscaping()
+                    .create();
+            String temp;
+            temp = "UserList";
+            File tempFile = new File(temp + ".json");
+            try {
+                FileWriter a = new FileWriter(tempFile);
+                tempFile.createNewFile();
+                gson1.toJson(users, a);
+                a.close();
 
+            } catch (IOException | IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public void storeUserList(){
         for (int i =0; i < Users.size(); i++)
         {
