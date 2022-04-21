9924dacbf9a22cb6925199a8f28c5a8650dbbafc
leela <lmh0030@uah.edu>
2022-04-21 01:22:14 -0500

    loginpage revert to include enter as guest button

--- View/LoginPage.java
+++ View/LoginPage.java
@@ -4,25 +4,31 @@ import com.company.Model.Login;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Objects;
 
-public class LoginPage implements ActionListener {
+/**
+ * LoginPage is the first thing the user sees when opening the app, it provides a portal to log in to personal GoodViews account
+ */
+public class LoginPage {
 
-    private static JLabel logoLabel;
-    private static JLabel usernameLabel;
     private static JTextField usernameText;
-    private static JLabel passwordLabel;
     private static JPasswordField passwordText;
     private static JButton loginButton;
     private static JButton createAccountButton;
-    private static JLabel success;
+    private static JButton guestUserButton;
     static JFrame frame;
     static JPanel panel;
 
+    /**
+     * default constructor
+     */
     public static void LoginPage() {
         // constructor for main
     }
+
+    /**
+     * draws login page components
+     */
     public void login() {
 
         //Creates Login Window
@@ -35,14 +41,14 @@ public class LoginPage implements ActionListener {
         panel.setLayout(null);
 
         //Creates GoodViews Logo
-        logoLabel = new JLabel("GoodViews");
+        JLabel logoLabel = new JLabel("GoodViews");
         logoLabel.setBounds(120, 30, 150, 25);
         logoLabel.setFont(new Font("Serif", Font.BOLD, 24));
         logoLabel.setForeground(Color.orange);
         panel.add(logoLabel);
 
         //Creates username prompt
-        usernameLabel = new JLabel("Username");
+        JLabel usernameLabel = new JLabel("Username");
         usernameLabel.setBounds(20, 80, 80, 25);
         usernameLabel.setForeground(Color.orange);
         panel.add(usernameLabel);
@@ -53,7 +59,7 @@ public class LoginPage implements ActionListener {
         panel.add(usernameText);
 
         // Creates password prompt
-        passwordLabel = new JLabel("Password");
+        JLabel passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(20, 120, 80, 25);
         passwordLabel.setForeground(Color.orange);
         panel.add(passwordLabel);
@@ -64,46 +70,85 @@ public class LoginPage implements ActionListener {
         panel.add(passwordText);
 
         // Creates Login Button
-        loginButton = new JButton("Login");
-        loginButton.setBounds(10, 160, 80, 25);
-        loginButton.setForeground(Color.orange);
-        loginButton.addActionListener(new LoginPage());
+        LoginButton();
         panel.add(loginButton);
 
         // Create Account Button
-        createAccountButton = new JButton("Create Account");
-        createAccountButton.setForeground(Color.orange);
-        createAccountButton.setBounds(100, 160, 165, 25);
+        CreateAccountButton();
         panel.add(createAccountButton);
 
-        // Creates Success response if user entry matches records
-        success = new JLabel("");
-        success.setBounds(10, 180, 300, 25);
-        panel.add(success);
-        success.setText("");
+        //Creates Guest Button
+        GuestUserButton();
+        panel.add(guestUserButton);
 
         frame.setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String userName = usernameText.getText();
-        String password = passwordText.getText();
-        Login log = new Login();
-        log.loadUserList();
-        if (log.checkInfo(userName, password) == false){
-            JLabel fail = new JLabel("Incorrect Username or Password");
-            fail.setBounds(90, 55, 300, 25);
-            fail.setForeground(Color.yellow);
-            panel.add(fail);
-            frame.repaint();
-        }
-        else {
-            panel.setVisible(false);
-            MainPage.createPage(frame, null, null, log.getCurrentUser());
-        }
-
+    /**
+     * creates login button with action listener
+     */
+    void LoginButton() {
+        loginButton = new JButton("Login");
+        loginButton.setBounds(190, 160, 75, 25);
+        loginButton.setForeground(Color.orange);
+        loginButton.addActionListener(e -> {
+            String userName = usernameText.getText();
+            String password = passwordText.getText();
+            Login.loadUserList();
+            if (!Login.checkInfo(userName, password)){
+                JLabel fail = new JLabel("Incorrect Username or Password");
+                fail.setBounds(90, 55, 300, 25);
+                fail.setForeground(Color.yellow);
+                panel.add(fail);
+                frame.repaint();
+            }
+            else {
+                panel.setVisible(false);
+                MainPage.createPage(frame, null, null, Login.getCurrentUser());
+            }
+        });
     }
+
+    /**
+     * creates create account button with action listener
+     */
+    void CreateAccountButton() {
+        createAccountButton = new JButton("Create Account");
+        createAccountButton.setForeground(Color.orange);
+        createAccountButton.setBounds(25, 225, 150, 25);
+        createAccountButton.addActionListener(e -> {
+            String user = usernameText.getText();
+            String pass = passwordText.getText();
+            if (Objects.equals(user, "") || Objects.equals(pass, "")) {
+                JLabel fail = new JLabel("Enter Username and Password to Create Account");
+                fail.setBounds(50, 55, 300, 25);
+                fail.setForeground(Color.yellow);
+                panel.add(fail);
+                frame.repaint();
+            }
+            else {
+                Login.createAccount(user, pass);
+                panel.setVisible(false);
+                if (Login.checkInfo(user, pass)) { MainPage.createPage(frame, null, null, Login.getCurrentUser()); }
+            }
+
+        });
+    }
+
+    /**
+     * creates enter as guest button with action listener
+     */
+    void GuestUserButton() {
+        guestUserButton = new JButton("Enter as Guest");
+        guestUserButton.setBounds(205, 225, 150, 25);
+        guestUserButton.setForeground(Color.orange);
+        guestUserButton.addActionListener(e -> {
+            Login.enterAsGuest(null,null); //change?
+            panel.setVisible(false);
+            MainPage.createPage(frame, null, null, Login.getCurrentUser());
+        });
+    }
+
 }
 
 
@@ -130,26 +175,8 @@ public class LoginPage implements ActionListener {
             log.loadProfile("Willsure");
             MainPage.createPage(frame, null, log.getUser());
         }
+
         else {
             success.setText("");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- */
+*/
